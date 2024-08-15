50002f37bb043d2431ca12add56092f3e0e33794
michaelwithlove <dmigueleduardo@gmail.com>
2024-08-15 14:13:05 -0300

    actualizacion de JSON y localStorage

--- JSON/main.js
+++ JSON/main.js
@@ -1,17 +1,25 @@
-//const arrayFrutas = ['frutilla', 'mango', 'manzana' ]
+const arrayFrutas = ['frutilla', 'mango', 'manzana' ]
 
 //JSON.stringify()
 
-//const jsonFrutas = JSON.stringify(arrayFrutas) //PERMITE CONVERTIR DATOS JS A DATOS JSON
+const jsonFrutas = JSON.stringify(arrayFrutas) //PERMITE CONVERTIR DATOS JS A DATOS JSON
 
 //console.log (arrayFrutas) //MUESTRA LOS DATOS EN FORMATO JS
-
+//
 //console.log (jsonFrutas) //MUESTRA LOS DATOS EN FORMATO JSON
 
 //JSON.parse()
 
 const jsonObjeto =  '{"nombre": "Juan Pérez", "edad": 30, "email": "juan.perez@example.com"}'
-console.log(jsonObjeto.nombre) //MUESTRA DATOS JSON
+//console.log(jsonObjeto.nombre) //MUESTRA DATOS JSON
 
 const Objeto = JSON.parse(jsonObjeto) //PERMITE CONVERTIR DATOS JSON A JS
-console.log(Objeto.nombre) //MUESTRA DATOS JS
+//console.log(Objeto.nombre) //MUESTRA DATOS JS
+
+//localStorage
+
+localStorage.setItem('nombre', 'Miguel')
+localStorage.setItem('apellido', 'Díaz')
+
+console.log(localStorage.getItem('nombre'))
+localStorage.removeItem('apellido')
