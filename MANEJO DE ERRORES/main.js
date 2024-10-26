dc88b6697e7af3cb37de5c3653a6e70c6df9092e
michaelwithlove <dmigueleduardo@gmail.com>
2024-10-26 16:30:25 -0300

    funciones de orden superior

--- MANEJO DE ERRORES/main.js
+++ MANEJO DE ERRORES/main.js
@@ -14,7 +14,22 @@ const numeros = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
 //forEach
 const sumarDos = num => console.log(num + 2)
-numeros.forEach(sumarDos)
+
+const arrayForEach = numeros.forEach(sumarDos)
+const arrayMap = numeros.map(sumarDos)
+
+const pares = numeros.filter(num => num%2 === 0) //el %2 es como decir "el resto 2 o mudolo 2"
+//console.log(pares)
+
+const suma = numeros.reduce((acumulador, valorActual) => acumulador + valorActual, 0) //es decir el acumulador inicial sera 0 y 5 el valor actual | 0, 5 | 5, 10 | 15, 20
+//console.log(suma)
+
+const numeritos = [1, 2, 3];
+const suma2 = numeritos.reduce((acumulador, valorActual) => acumulador + valorActual, 1)
+console.log(suma2)
+
+//console.log(arrayForEach)
+//console.log(arrayMap)
 
 //numeros.forEach(num => {
 // console.log(num)
