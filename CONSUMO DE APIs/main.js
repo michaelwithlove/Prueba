291400b4793998585ac43f44a01026e161318728
michaelwithlove <dmigueleduardo@gmail.com>
2024-11-05 16:32:02 -0300

    Consumo de API

--- CONSUMO DE APIs/main.js
+++ CONSUMO DE APIs/main.js
@@ -1,16 +1,24 @@
+import { Key } from "./Key.js";
+
+const formulario = document.querySelector('form')
 const container = document.querySelector('.container')
 
-container.innerHTML = 'Hola mundo!'
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-    .then(Response => Response.json())
-    //.then(data => console.log(data.results))
-    .then(data => {
-        let datos = ''
-        data.results.forEach(element => {
-            console.log(element.name)
-            datos += `<h3>${element.name}</h3>` //+= es concatenacion de texto, ponemos las ``para manipular la visualizacion del innerHtml? y llamamos el elemento con ${}
-        });
-        container.innerHTML = datos
-    })
-    .catch(err => console.error(err))
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault ()
+    const data = Object.fromEntries(new FormData(event.target))
+    
+fetch(`https://www.omdbapi.com/?apikey=${Key}&type=movie&s=${data.Buscar}`)
+        .then(Response => Response.json())
+        .then(datos => {
+            let datosParseados = ''
+            datos.Search.forEach(movie => {
+                datosParseados += `<div>
+                                        <h3>${movie.Title}</h3>
+                                        <h4>${movie.Year}</h4>
+                                        <img src="${movie.Poster}" alt="" />
+                                    </div>`
+            });
+            container.innerHTML = datosParseados
+        })
+        .catch(err => console.error(err))
+})
